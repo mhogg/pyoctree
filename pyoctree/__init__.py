28e00395cd29dee1449ec522b55d08f68518eb70
Michael Hogg <michael.christopher.hogg@gmail.com>
2017-05-08 21:38:34 +1000

    Fix import bug in Python 3

--- pyoctree/__init__.py
+++ pyoctree/__init__.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 
-# Copyright (C) 2015 Michael Hogg
+# Copyright (C) 2017 Michael Hogg
 
 # This file is part of pyoctree - See LICENSE.txt for information on usage and redistribution
 
 # Version
-import version
+from .version import __version__
 __version__ = version.__version__
