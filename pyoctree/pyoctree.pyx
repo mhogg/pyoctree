1e0ad24e56e8c0ab7789fddcd582d7ee01914c75
Michael Hogg <michael.christopher.hogg@gmail.com>
2017-05-09 10:07:00 +1000

    Update version and copyright

--- pyoctree/pyoctree.pyx
+++ pyoctree/pyoctree.pyx
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Copyright (C) 2015 Michael Hogg
+# Copyright (C) 2017 Michael Hogg
 
 # This file is part of pyoctree - See LICENSE.txt for information on usage and redistribution
 
