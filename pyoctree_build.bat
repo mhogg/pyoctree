f7f22adcb9793c1c6166d67fd07c1479065f3451
Michael Hogg <michael.christopher.hogg@gmail.com>
2017-05-09 10:07:30 +1000

    Use MSVC 14.0 (2015) for compiling Python 3.6

--- pyoctree_build.bat
+++ pyoctree_build.bat
@@ -3,6 +3,9 @@ REM @call "C:\Users\michaelh3\AppData\Local\Programs\Common\Microsoft\Visual C++
 REM Set environ as workaround for bugs (See http://stackoverflow.com/questions/2817869/error-unable-to-find-vcvarsall-bat and http://bugs.python.org/issue23246)
 REM SET DISTUTILS_USE_SDK=1
 REM SET MSSdk=1
+
+@call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x86_amd64
+
 REM Test build
 python setup.py sdist bdist_wheel
 REM Upload to PyPi
